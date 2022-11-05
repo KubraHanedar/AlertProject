6c762cf87eb93db58785a1f920711d4a0bd9c1f5
Kubra Hanedar <the.haneda@gmail.com>
2022-11-05 22:03:53 +0300

    alert project komutları yazıldı.

--- AlertProject/ViewController.swift
+++ AlertProject/ViewController.swift
@@ -20,14 +20,41 @@ class ViewController: UIViewController {
     }
     
     @IBAction func singupButton(_ sender: Any) {
+        /*
+         
+         
+         let alert = UIAlertController(title: "Error!", message: "Username not found!", preferredStyle: UIAlertController.Style.alert)
+         let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default) { (UIAlertAction) in
+         print("button clicked")
+         }
+         alert.addAction(okButton)
+         self.present(alert, animated: true, completion: nil)
+         */
         
-        let alert = UIAlertController(title: "Error!", message: "Username not found!", preferredStyle: UIAlertController.Style.alert)
-        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default) { (UIAlertAction) in
-            print("button clicked")
-            }
-        alert.addAction(okButton)
-        self.present(alert, animated: true, completion: nil)
+        if nameText.text == "" {
+            let alert = UIAlertController(title: "Error", message: "Username Not Found", preferredStyle: UIAlertController.Style.alert)
+            let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default,handler: nil)
+            alert.addAction(okButton)
+            self.present(alert, animated: true, completion: nil)
+        } else if passwordText.text == "" {
+            let alert = UIAlertController(title: "Error", message: "Password Not Found", preferredStyle: UIAlertController.Style.alert)
+            let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default,handler: nil)
+            alert.addAction(okButton)
+            self.present(alert, animated: true, completion: nil)
+        }  else if passwordText.text != secondPasswordText.text {
+            let alert = UIAlertController(title: "Error", message: "Password do not mach", preferredStyle: UIAlertController.Style.alert)
+            let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default,handler: nil)
+            alert.addAction(okButton)
+            self.present(alert, animated: true, completion: nil)
+        } else {
+            let alert = UIAlertController(title: "Success", message: "User OK", preferredStyle: UIAlertController.Style.alert)
+            let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default,handler: nil)
+            alert.addAction(okButton)
+            self.present(alert, animated: true, completion: nil)
+        }
+        
     }
+    
     
 }
 
